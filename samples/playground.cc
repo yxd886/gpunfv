f8fce3e9db8d64070153b32a85297573530064e0
xiaodongyi <409070169@qq.com>
2018-03-27 16:07:44 +0800

    remove assert

--- samples/playground.cc
+++ samples/playground.cc
@@ -1032,7 +1032,7 @@ int main(int ac, char** av) {
             return forwarders.invoke_on_all(&forwarder::mica_test, 1);
         })*/.then([]{
             return forwarders.invoke_on_all(&forwarder::configure, 1);
-        }).then([&netstar_pools]{
+        }).then([&local_netstar_pools]{
                 for(unsigned int i = 0; i<local_netstar_pools.size();i++){
                     gpu_mem_map(local_netstar_pools[i],mbufs_per_queue_tx*inline_mbuf_size+mbuf_cache_size+sizeof(struct rte_pktmbuf_pool_private));
                 }
