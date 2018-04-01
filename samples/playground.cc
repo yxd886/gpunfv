4b38de06c25444d5c2a16e8ac30c0670facf15fa
Yi Xiaodong <409070169@qq.com>
2018-04-01 16:35:43 +0800

    Finish using copy OK, debug finish

--- samples/playground.cc
+++ samples/playground.cc
@@ -728,7 +728,7 @@ public:
             if(GPU_BATCH_SIZE!=1){
                 sort(_flows[index].begin(),_flows[index].end(),CompLess);
                 partition=get_partition(index);
-               // partition=_flows[index].size()*5/6;
+                partition=_flows[index].size()/2;
                 if(PRINT_TIME)std::cout<<"Total flow_num:"<<_flows[index].size()<<std::endl;
                 if(PRINT_TIME)printf("partition: %d\n",partition);
             }
