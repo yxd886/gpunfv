06645973cd2267f0900f0f2136e23fe84113bfc0
xiaodongyi <409070169@qq.com>
2018-04-02 10:58:13 +0800

    optimize gpu code

--- samples/gpu_test.cu
+++ samples/gpu_test.cu
@@ -132,7 +132,7 @@ __global__ void gpu_nf_logic(char** pkt_batch, char **state_batch, char *extra_i
  //printf("gpu_nf_logic(): state->_dfa_id = %d\n", ((struct ips_flow_state *)state_batch[id])->_dfa_id);
 		//gpu_nf_logic_impl(pkts[i], state_batch[id]);
 		//ips_detect((char*)pkts[i].pkt, &state_ptr[id], (struct gpu_IPS *)extra_info);
-		//ips_detect(pkts[i], (struct ips_flow_state *)state_batch[id], (struct gpu_IPS *)extra_info);
+		ips_detect(pkts[i], (struct ips_flow_state *)state_batch[id], (struct gpu_IPS *)extra_info);
 //	printf("id = %d, end\n", id);	
 	}
 	//printf("GPU: gpu_states[%d].dfa_id: %d\n",id,state_ptr[id]._dfa_id);
