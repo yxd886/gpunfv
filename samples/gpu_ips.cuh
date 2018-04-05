0a75449bbc6e169de11a863b36fece835da169fa
xiaodongyi <409070169@qq.com>
2018-04-05 15:46:30 +0800

    try map

--- samples/gpu_ips.cuh
+++ samples/gpu_ips.cuh
@@ -13,8 +13,8 @@
 #include <netinet/in.h>
 
 #include "common.cuh"
-#include "nf/aho-corasick/fpp.h"
-#include "nf/aho-corasick/aho.h"
+#include "/home/net/gpunfv/seastar/nf/aho-corasick/fpp.h"
+#include "/home/net/gpunfv/seastar/nf/aho-corasick/aho.h"
 
 #define MAX_MATCH 8192
 #define MAX_PKT_SIZE 1500
