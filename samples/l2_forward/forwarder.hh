8768a9d4dd7f3ddd60611b7f98de1a36dddbc85f
xiaodongyi <409070169@qq.com>
2018-04-10 16:33:28 +0800

    test gpu_processing time

--- samples/l2_forward/forwarder.hh
+++ samples/l2_forward/forwarder.hh
@@ -511,7 +511,14 @@ public:
                     gpu_sync(stream);
                 started[lcore_id] = steady_clock_type::now();
                 }
-                gpu_memcpy_async_h2d(dev_gpu_pkts,gpu_pkts[index],ngpu_pkts,stream);
+                //gpu_memcpy_async_h2d(dev_gpu_pkts,gpu_pkts[index],ngpu_pkts,stream);
+                for(int i = 0; i < partition; i++){
+
+                    for(int j = 0; j < (int)_flows[index][i]->packets[index].size(); j++){
+
+                        gpu_memcpy_async_h2d(dev_gpu_pkts[i*max_pkt_num_per_flow+j].pkt,reinterpret_cast<char*>(_flows[index][i]->packets[index][j].get_header<ether_hdr>(0)),_flows[index][i]->packets[index][j].len(),stream);
+                    }
+                }
 
                 if(gpu_time){
                     gpu_sync(stream);
