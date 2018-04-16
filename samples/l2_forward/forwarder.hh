5cf42e9bd7c2de3dda7c339612308531fa2df240
xiaodongyi <409070169@qq.com>
2018-04-16 11:19:52 +0800

    add enqueue time

--- samples/l2_forward/forwarder.hh
+++ samples/l2_forward/forwarder.hh
@@ -459,7 +459,7 @@ public:
                 simple_stoped[lcore_id] = steady_clock_type::now();
                 auto simple_elapsed = simple_stoped[lcore_id] - simple_started[lcore_id];
 
-                _profile_elements.cpu_enqueue_time = simple_elapsed;
+                _profile_elements.cpu_enqueue_time = static_cast<double>(simple_elapsed.count() / 1.0);
                 if(print_simple_time) printf("Simple Enqueuing time: %f\n", static_cast<double>(simple_elapsed.count() / 1.0));
                 simple_started[lcore_id] = steady_clock_type::now();
             }
