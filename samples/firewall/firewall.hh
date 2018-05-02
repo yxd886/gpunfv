62a17ea9531b76307ff2c19e9289f8e228273079
xiaodongyi <409070169@qq.com>
2018-05-02 12:13:04 +0800

    pretech filrewall

--- samples/firewall/firewall.hh
+++ samples/firewall/firewall.hh
@@ -133,9 +133,9 @@ private:
 		bool drop;
 		uint32_t i;
 
-		for( i = 0; i<rules.size();i++){
-		    rte_prefetch0((void*)&rules[i]);
-		}
+
+        rte_prefetch0((void*)&rules[0]);
+
 		// Match rules
 		for(i = 0; i < rules.size(); i++){
 			Rule temp = rules[i];	// GPU vector access must via this copy
