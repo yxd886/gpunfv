7343c812b45d78ef14dd53ad2fbe5a4b3e41ceaa
Yi Xiaodong <409070169@qq.com>
2018-05-06 21:35:05 +0800

    load_balancer

---
+++ samples/load_balabcer/nf.cuh
@@ -0,0 +1,10 @@
+#ifndef NF_CUH
+#define NF_CUH
+
+#include "load_balancer.cuh"
+
+typedef class load_balancer  NF;						// specify NF class
+typedef class load_balancer_flow_state nf_flow_state;	// specify NF state class
+typedef class Rules Infos;
+
+#endif // NF_CUH
