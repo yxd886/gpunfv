770412a6089daa9cbc330281fbe71f0ebf3aa124
Yi Xiaodong <409070169@qq.com>
2018-05-06 18:30:48 +0800

    flow monitor

---
+++ samples/flow_monitor/nf.hh
@@ -0,0 +1,9 @@
+#ifndef NF_HH
+#define NF_HH
+
+#include "flow_monitor.hh"
+
+typedef class flow_monitor NF;						// specify NF class
+typedef class flow_monitor_flow_state nf_flow_state;	// specify NF state class
+
+#endif
