841f383445fa607f202ec2a50ac41c919fe0a28b
xiaodongyi <409070169@qq.com>
2018-05-08 11:00:45 +0800

    add fm_fw_lb

---
+++ samples/load_balancer/nf.hh
@@ -0,0 +1,9 @@
+#ifndef NF_HH
+#define NF_HH
+
+#include "load_balancer.hh"
+
+typedef class load_balancer NF;						// specify NF class
+typedef class load_balancer_flow_state nf_flow_state;	// specify NF state class
+
+#endif
