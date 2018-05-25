dda9597d904182355e1dbd3e5ec65f1cf31107ae
xiaodongyi <409070169@qq.com>
2018-05-25 19:14:38 +0800

    gpu ok

--- samples/proxy/forwarder.hh
+++ samples/proxy/forwarder.hh
@@ -236,7 +236,7 @@ public:
     const uint16_t ip_packet_len_max = 65535;
 
     void time_trigger_schedule(){
-        if (_pkt_counter==0||_batch._period_profile) return;
+        if (_batch._period_profile) return;
         _pkt_counter=0;
         _batch.current_idx=!_batch.current_idx;
         printf("lcore_id: %d, trigger\n",_lcore_id);
