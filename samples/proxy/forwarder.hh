97de23ff381fdd1f3241172221f2ea7ea5a0313c
xiaodongyi <409070169@qq.com>
2018-05-29 11:43:46 +0800

    ok

--- samples/proxy/forwarder.hh
+++ samples/proxy/forwarder.hh
@@ -5,7 +5,21 @@
 #include <omp.h>
 #include <future>
 #include "../include/gpu_interface.hh"
+extern"C"{
+#include <event2/bufferevent_ssl.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+#include <event2/event_struct.h>
 
+#include "util-internal.h"
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
+#include "openssl-compat.h"
+
+}
 extern uint64_t _batch_size;
 extern uint64_t print_time;
 extern uint64_t gpu_time;
