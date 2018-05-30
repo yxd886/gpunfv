ec7e6d61ece5ff58fd58ced28dd174f51ed0f663
xiaodongyi <409070169@qq.com>
2018-05-30 15:26:09 +0800

    add nf

--- samples/proxy/l2_forward.cc
+++ samples/proxy/l2_forward.cc
@@ -31,6 +31,8 @@ extern"C"{
 #include <event2/event.h>
 #include <event2/bufferevent_ssl.h>
 #include <event2/bufferevent.h>
+#include <event2/bufferevent_compat.h>
+#include <event2/bufferevent_struct.h>
 #include <event2/buffer.h>
 #include <event2/listener.h>
 #include <event2/util.h>
