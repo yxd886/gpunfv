7a8a48d15fb90f333f4fd07f100b77de04f16ce3
xiaodongyi <409070169@qq.com>
2018-06-07 15:51:35 +0800

    aaa

--- samples/AES/aes.cuh
+++ samples/AES/aes.cuh
@@ -179,9 +179,10 @@ public:
         // Initialize context
         struct AES_ctx ctx;
         AES_init_ctx_iv(&ctx, state->key, state->iv);
-        AES_CBC_encrypt_buffer(&ctx, buffer, len_padding);
-        AES_init_ctx_iv(&ctx, state->key, state->iv);
-        AES_CBC_decrypt_buffer(&ctx, buffer, len_padding);
+        if(state->is_encryption)
+      	 	AES_CBC_encrypt_buffer(&ctx, buffer, len_padding);
+		else
+    		    AES_CBC_decrypt_buffer(&ctx, buffer, len_padding);
 
         // copy back
        // if(len % AES_BLOCKLEN != 0) {
