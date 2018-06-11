4d39f5307c0db3a6bd97e2e8ef36af101bde59ed
xiaodongyi <409070169@qq.com>
2018-06-11 10:43:20 +0800

    eval ssl

--- samples/AES/aes.cuh
+++ samples/AES/aes.cuh
@@ -179,10 +179,12 @@ public:
         // Initialize context
         struct AES_ctx ctx;
         AES_init_ctx_iv(&ctx, state->key, state->iv);
-        if(state->is_encryption)
-      	 	AES_CBC_encrypt_buffer(&ctx, buffer, len_padding);
-		else
-    		    AES_CBC_decrypt_buffer(&ctx, buffer, len_padding);
+        if(state->is_encryption){
+        		AES_CBC_encrypt_buffer(&ctx, buffer, len_padding);
+        }else{
+        		AES_CBC_decrypt_buffer(&ctx, buffer, len_padding);
+        }
+    		    
 
         // copy back
        // if(len % AES_BLOCKLEN != 0) {
