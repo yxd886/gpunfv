4825a5f5d6d15a84b398eb10effed69419220913
duanjp8617 <duanjp8617@gmail.com>
2018-06-12 15:25:41 +0800

    change name

--- samples/WAF/nf.hh
+++ samples/WAF/nf.hh
@@ -1,7 +1,7 @@
 #ifndef NF_HH
 #define NF_HH
 
-#include "ips.hh"
+#include "waf_ac.hh"
 
 using NF = IPS;                         // specify NF class
 using nf_flow_state = ips_flow_state;   // specify NF state class
