7d1990580b6609c8af4832fc81f98c148c479acd
duanjp8617 <duanjp8617@gmail.com>
2018-06-12 15:53:55 +0800

    On GPU side, change the name

--- samples/WAF/nf.cuh
+++ samples/WAF/nf.cuh
@@ -3,9 +3,9 @@
 
 #include "ips.cuh"
 
-typedef class IPS NF;						// specify NF class
-typedef class ips_flow_state nf_flow_state;	// specify NF state class
-typedef class gpu_IPS Infos;
+typedef class WAF_AC NF;						// specify NF class
+typedef class waf_ac_flow_state nf_flow_state;	// specify NF state class
+typedef class gpu_WAF_AC Infos;
 
 
 #endif // NF_CUH
