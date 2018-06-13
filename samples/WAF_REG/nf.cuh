06625ea19b5ef51a96751ce2b5b2e44074d04793
duanjp8617 <duanjp8617@gmail.com>
2018-06-13 11:08:19 +0800

    test

--- samples/WAF_REG/nf.cuh
+++ samples/WAF_REG/nf.cuh
@@ -1,11 +1,11 @@
 #ifndef NF_CUH
 #define NF_CUH
 
-#include "ips.cuh"
+#include "waf.cuh"
 
-typedef class IPS NF;						// specify NF class
-typedef class ips_flow_state nf_flow_state;	// specify NF state class
-typedef class gpu_IPS Infos;
+typedef class WAF NF;						// specify NF class
+typedef class waf_flow_state nf_flow_state;	// specify NF state class
+typedef class WAF Infos;
 
 
 #endif // NF_CUH
