df82116b309a0c244ba57e8425f8f72082d39a4e
duanjp8617 <duanjp8617@gmail.com>
2018-06-13 10:54:02 +0800

    test

--- samples/WAF_REG/waf.hh
+++ samples/WAF_REG/waf.hh
@@ -14,21 +14,24 @@
 #include "reg_match.hh"
 
 #define REG_EXPR_NUM 4
-
 static const char *reg_expr1 = "1+2+3";
 static const char *reg_expr2 = "w+t+f";
 static const char *reg_expr3 = "4+5+6";
 static const char *reg_expr4 = "H+a+H";
-
 static const char *reg_expr_array[REG_EXPR_NUM] = {reg_expr1, reg_expr2, reg_expr3, reg_expr4};
+
+#define MAX_STR_LENGTH 128;
 
 struct waf_flow_state {
     // A pointer to the start of the method string
-	char *method;
+	// char *method;
+
 	// Size of the method string
     size_t method_len;
+
     // A pointer to the start of the path string
-    char *path;
+    // char *path;
+
     // Size of the path string
     size_t path_len;
     // HTTP minor version
@@ -37,6 +40,8 @@ struct waf_flow_state {
     struct phr_header headers[4];
     // Total number of the parsed headers
     size_t num_headers;
+    // A char array which is used to store a string for matching.
+    char str[MAX_STR_LENGTH];
     // Whether we have detected a match for a particular request
     bool is_reg_matched;
 };
@@ -70,8 +75,14 @@ public:
 									state->headers,
 									&state->num_headers,
 									0);
+
 		// Remove this assert if it doesn't work.
 		assert(ret == buf_len);
+
+		// Do some post processing for this HTTP request
+		// ....
+
+		// Then apply regular expression matching.
 
 
 	}
